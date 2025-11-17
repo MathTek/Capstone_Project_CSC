0c4d39534f88343dcfdc82d76fe07c569e3f58f8
MathTek <mathieu.rasoanaivo@epitech.eu>
2025-11-17 19:04:46 +0100

    Feat: Setup Project structure Front,Back,Extension ready to dev

---
+++ WebApp/Backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/data")
+def get_data():
+    return {"values": [10, 30, 50, 80]}
